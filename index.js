ac70de1c95d5b175155168140c9d10a8b523629d
mirehiko <mirehiko@mirehiko-B450M-S2H.lan>
2020-08-17 13:06:04 +0500

    Get params from command line

--- index.js
+++ index.js
@@ -1,38 +1,12 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const optimist = require('optimist');
 
-var server = new http.Server;
 
-// console.log('ASLDJLADJLASDJ')
+// console.log(optimist.argv)
 
-server.listen(3000, '127.0.0.1'); //localhost
+var message = optimist.argv.message;
 
-// var counter = 0;
-console.log('Server is running')
+console.log(`Hello ${message}`);
 
-server.on('request', function(req, res) {
-    // res.end(`Hello world ${++counter}`);
-
-    let parsedUrl = url.parse(req.url, true); //true означает что поле url нужно представить в виде объекта
-    fs.readFile( `${getPageByPath(parsedUrl.pathname)}.html`, function(err, data) {
-        if (err) {
-            console.log(new Error(err), err);
-        };
-        res.end(data);
-    });
-
-    // res.end(parserUrl.query.q);
-});
-
-function getPageByPath(path) {
-    switch(path) {
-        case '/':
-        case '/home': 
-            return 'index';
-        case '/about': 
-            return 'about';
-        case '/404': 
-            return '404';
-    }
-}
+// for (let i = 2; i < process.argv.length; i++) {
+//     console.log(process.argv[i])
+// }
