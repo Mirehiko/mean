ea75732ef8edcc275e1041d16383ffef98a4e720
mirehiko <mirehiko@mirehiko-B450M-S2H.lan>
2020-08-25 14:52:57 +0500

    - Added markdown for site layout;

--- client/src/app/shared/layouts/site-layout/site-layout.component.ts
+++ client/src/app/shared/layouts/site-layout/site-layout.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthGuard } from '../../classes/auth.guard';
+import { MaterialService } from '../../classes/material.service';
 
 @Component({
   selector: 'app-site-layout',
   templateUrl: './site-layout.component.html',
   styleUrls: ['./site-layout.component.scss']
 })
-export class SiteLayoutComponent implements OnInit {
+export class SiteLayoutComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('floating') floatingRef: ElementRef;
+
+  links = [
+    {url: '/overview', name: 'Обзор'},
+    {url: '/analytics', name: 'Аналитика'},
+    {url: '/history', name: 'История'},
+    {url: '/order', name: 'Добавить заказ'},
+    {url: '/categories', name: 'Ассортимент'},
+  ]
+
+  constructor(
+    private auth: AuthGuard,
+    private router: Router
+  ) { }
+
+  ngAfterViewInit(): void {
+    MaterialService.initFloatingButton(this.floatingRef);
+  }
 
   ngOnInit(): void {
   }
 
+  logout(event: Event) {
+    event.preventDefault();
+    this.router.navigate(['/login']);
+  }
 }
