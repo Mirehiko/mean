61663657217f7e435e7d7de2e4e315cc94cd2099
mirehiko <mirehiko@mirehiko-B450M-S2H.lan>
2020-08-25 16:47:47 +0500

    categories page

--- client/src/app/categories-page/categories-page.component.ts
+++ client/src/app/categories-page/categories-page.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { Category } from '../shared/interfaces';
+import { CategoriesService } from '../shared/services/categories.service';
 
 @Component({
   selector: 'app-categories-page',
   templateUrl: './categories-page.component.html',
   styleUrls: ['./categories-page.component.scss']
 })
-export class CategoriesPageComponent implements OnInit {
+export class CategoriesPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  cSub: Subscription;
+  categories$: Observable<Category[]>;
+  // categories: Category[] = [];
+  // loading = false;
+
+  constructor(
+    private categoriesService: CategoriesService
+  ) { }
+
+  ngOnDestroy(): void {
+    if (this.cSub) {
+      this.cSub.unsubscribe();
+    }
+  }
 
   ngOnInit(): void {
+    this.categories$ = this.categoriesService.fetch();
+    // this.loading = true;
+    // this.cSub = this.categoriesService.fetch().subscribe(categories => {
+    //   this.categories = categories;
+    //   this.loading = false;
+    // });
   }
 
 }
