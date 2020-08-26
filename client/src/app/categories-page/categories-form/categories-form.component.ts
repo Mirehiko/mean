435d2278c246343d0ea7e186ad71f8817c13026e
mirehiko <mirehiko@mirehiko-B450M-S2H.lan>
2020-08-26 12:23:02 +0500

    - Added an ability to upload images; - Created a functional for remove categories;

--- client/src/app/categories-page/categories-form/categories-form.component.ts
+++ client/src/app/categories-page/categories-form/categories-form.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { CategoriesService } from '../../shared/services/categories.service';
 import { switchMap } from 'rxjs/operators';
 import { of } from 'rxjs';
-import { error } from 'protractor';
 import { Category } from '../../shared/interfaces';
 import { MaterialService } from 'src/app/shared/classes/material.service';
 
@@ -15,12 +14,16 @@ import { MaterialService } from 'src/app/shared/classes/material.service';
 })
 export class CategoriesFormComponent implements OnInit {
 
+  @ViewChild('input') inputRef: ElementRef;
+  image: File;
   isNew = true;
   form: FormGroup;
+  imagePreview: string | ArrayBuffer = '';
   category: Category;
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private categoriesService: CategoriesService
   ) { }
 
@@ -39,19 +42,70 @@ export class CategoriesFormComponent implements OnInit {
         }
         return of(null);
       })
-    ).subscribe(category => {
+    ).subscribe((category: Category) => {
       if (category) {
+        this.category = category;
         this.form.patchValue({
           name: category.name
         });
+        this.imagePreview = category.imageSrc;
         MaterialService.updateTextInputs();
       }
       this.form.enable();
     }, error => MaterialService.toast(error.error.message));
   }
 
-  onSubmit() {
+  deleteCategory() {
+    const decision = window.confirm(`Вы уверены, что хотите удалить категорию ${this.category.name}?`);
 
+    if (decision) {
+      this.categoriesService.delete(this.category._id)
+        .subscribe(
+          response => MaterialService.toast(response.message),
+          error => MaterialService.toast(error.error.message),
+          () => this.router.navigate(['/categories'])
+        );
+    }
   }
 
+  onSubmit() {
+    this.form.disable();
+    let obs$;
+
+    if (this.isNew) {
+      obs$ = this.categoriesService.create(this.form.value.name, this.image);
+    }
+    else {
+      obs$ = this.categoriesService.update(this.category._id, this.form.value.name, this.image);
+    }
+
+    obs$.subscribe(
+      category => {
+        this.category = category;
+        MaterialService.toast('Изменения сохранены');
+        this.form.enable();
+      },
+      error => {
+        MaterialService.toast(error.error.message);
+        this.form.enable();
+      }
+    );
+  }
+
+  triggerClick() {
+    this.inputRef.nativeElement.click();
+  }
+
+  onFileUpload(event: any) {
+    const file = event.target.files[0];
+    this.image = file;
+
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    };
+
+    reader.readAsDataURL(file);
+  }
 }
