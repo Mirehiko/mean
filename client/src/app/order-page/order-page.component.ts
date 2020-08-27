e8d79babd9bd0c5d6fbbe4c2ae03622dea262c34
mirehiko <mirehiko@mirehiko-B450M-S2H.lan>
2020-08-27 17:28:55 +0500

    - Show production list; - Confugurated of formation order; - Save order to database;

--- client/src/app/order-page/order-page.component.ts
+++ client/src/app/order-page/order-page.component.ts
@@ -1,7 +1,10 @@
 import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { MaterialService, MaterialInstance } from '../shared/classes/material.service';
-import { OrderService } from '../shared/services/order.service';
+import { Order, OrderPosition } from '../shared/interfaces';
+import { OrdersService } from '../shared/services/orders.service';
+import { OrderService } from './order.service';
 
 @Component({
   selector: 'app-order-page',
@@ -14,10 +17,13 @@ export class OrderPageComponent implements OnInit, OnDestroy, AfterViewInit {
   @ViewChild('modal') modalRef: ElementRef;
   isRoot: boolean;
   modal: MaterialInstance;
+  pending = false;
+  oSub: Subscription;
 
   constructor(
     private router: Router,
-    private order: OrderService
+    public order: OrderService,
+    private ordersService: OrdersService
   ) { }
 
   ngOnInit(): void {
@@ -35,6 +41,9 @@ export class OrderPageComponent implements OnInit, OnDestroy, AfterViewInit {
 
   ngOnDestroy(): void {
     this.modal.destroy();
+    if (this.oSub) {
+      this.oSub.unsubscribe();
+    }
   }
 
   open() {
@@ -46,6 +55,25 @@ export class OrderPageComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   submit() {
-    this.modal.close();
+    this.pending = true;
+
+    const order: Order = {
+      list: this.order.list.map(item => {
+        delete item._id;
+        return item;
+      })
+    };
+
+    this.oSub = this.ordersService.create(order).subscribe(createdOrder => {
+      MaterialService.toast(`Заказ №${createdOrder.order} был добавлен`);
+    }, error => MaterialService.toast(error.error.message),
+    () => {
+      this.modal.close();
+      this.pending = false;
+    });
+  }
+
+  removePosition(orderPosition: OrderPosition) {
+    this.order.remove(orderPosition);
   }
 }
--- client/src/app/order-page/order-positions/order-positions.component.ts
+++ client/src/app/order-page/order-positions/order-positions.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { Position } from 'src/app/shared/interfaces';
+import { PositionService } from 'src/app/shared/services/position.service';
+import { OrderService } from 'src/app/order-page/order.service';
+import { MaterialService } from 'src/app/shared/classes/material.service';
 
 @Component({
   selector: 'app-order-positions',
@@ -7,9 +14,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderPositionsComponent implements OnInit {
 
-  constructor() { }
+  positions$: Observable<Position[]>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private positionService: PositionService,
+    private order: OrderService
+  ) { }
 
   ngOnInit(): void {
+    this.positions$ = this.route.params.pipe(
+      switchMap(
+        (params: Params) => {
+          return this.positionService.fetch(params['id']);
+        }
+      ),
+      map(
+        (positions: Position[]) => {
+          return positions.map(position => {
+            position.quantity = 1;
+            return position;
+          });
+        }
+      )
+    );
+  }
+
+  addToOrder(position: Position) {
+    MaterialService.toast(`Добавлено x${position.quantity}`);
+    this.order.add(position);
   }
 
 }
