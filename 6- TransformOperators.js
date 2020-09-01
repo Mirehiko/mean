517edbe25cb6113659d634d6f32850e9fee29cd1
mirehiko <mirehiko@mirehiko-B450M-S2H.lan>
2020-09-01 16:00:49 +0500

    transsform operators

---
+++ 6- TransformOperators.js
@@ -0,0 +1,42 @@
+function createSubscribe(name) {
+    return {
+        next(x) {
+            console.log(`${name}:`, x);
+        },
+        error(err) {
+            console.log(`Error:`, x);
+        },
+        complete() {
+            console.log(`${name}:`, 'Completed');
+        }
+    }
+}
+
+rxjs.interval(1000)
+    .pipe(
+        rxjs.operators.map(x => {
+            return x * 2;
+        }),
+        rxjs.operators.take(10)
+    )
+    .subscribe(createSubscribe('[operators] map'));
+
+rxjs.of('hello', 'world', 'wtf')
+    .pipe(
+        rxjs.operators.map(x => x.toUpperCase())
+    )
+    .subscribe(createSubscribe('[operators] map'));
+
+rxjs.fromEvent(document.querySelector('#les2Input1'), 'keyup')
+    .pipe(
+        // rxjs.operators.map(x => x.target.value),
+        rxjs.operators.pluck('target', 'value'),
+        rxjs.operators.map(x => x.toUpperCase()),
+        rxjs.operators.map(x => {
+            return {
+                value: x,
+                length: x.length
+            };
+        })
+    )
+    .subscribe(createSubscribe('[operators] map'));
