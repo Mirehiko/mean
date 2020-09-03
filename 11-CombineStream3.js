86a3e614b23edff69798b6b923b2cbab0ba745b3
mirehiko <mirehiko@mirehiko-B450M-S2H.lan>
2020-09-03 11:12:14 +0500

    Combine streams: zip, combineLatest

---
+++ 11-CombineStream3.js
@@ -0,0 +1,53 @@
+function createSubscribe(name) {
+  return {
+    next(x) {
+      console.log(`${name}:`, x);
+    },
+    error(err) {
+      console.log(`Error:`, err);
+    },
+    complete() {
+      console.log(`${name}:`, "Completed");
+    },
+  };
+}
+
+// // Part 1
+// const s1$ = rxjs.of('Hello');
+// const s2$ = rxjs.of('World');
+
+// rxjs.zip(s1$.pipe(rxjs.operators.delay(2000)), s2$.pipe(rxjs.operators.delay(5000)))
+//   .subscribe(createSubscribe('zip'));
+
+
+// // Part 2
+// const interval$ = rxjs.interval(1000);
+
+// rxjs
+//   .zip(
+//     interval$,
+//     interval$.pipe(rxjs.operators.take(3)),
+//     rxjs.of("Some text")
+//   )
+//   .subscribe(createSubscribe('zip2'));
+
+
+// // Part 3
+// const si1$ = rxjs.interval(1000);
+// const si2$ = rxjs.interval(500);
+
+// si1$
+//   .pipe(
+//     rxjs.operators.withLatestFrom(si2$),
+//     rxjs.operators.take(5)
+//   )
+//   .subscribe(createSubscribe('withLatestFrom'));
+
+// Part 3
+const t1$ = rxjs.timer(1000, 2000);
+const t2$ = rxjs.timer(2000, 2000);
+const t3$ = rxjs.timer(3000, 2000);
+
+rxjs.combineLatest(t1$, t2$, t3$)
+  .pipe(rxjs.operators.take(5))
+  .subscribe(createSubscribe('combineLatest'));
