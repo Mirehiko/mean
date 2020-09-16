8c1dfd7a49f066e62855e70b97b4c44edda3bf5d
mirehiko <mirehiko@mirehiko-B450M-S2H.lan>
2020-09-16 12:28:20 +0500

    format code and remove old access

--- 14-final.js
+++ 14-final.js
@@ -1,20 +1,6 @@
-// function getUserById(id) {
-//     const params = {
-//         access_token: '396e766f010ac5ba9eea7589c45366e9ed4099132313ca5a9af4be6fe26207259fbc9d4df9b5debc2197a',
-//         user_ids: id,
-//     };
-
-//     return $.ajax({
-//         url: 'https://api.vk.com/method/friends.getOnline?v=5.52&access_token=' + $.param(params),
-//         type: 'GET',
-//         dataType: 'JSONP'
-//     }).promise();
-// }
-
 function getUserById(id) {
   const params = {
-    access_token:
-      "place here your vkapp token",
+    access_token: "place here your vkapp token",
     user_ids: id,
     fields: "photo_big",
   };
@@ -26,20 +12,24 @@ function getUserById(id) {
   }).promise();
 }
 
-rxjs.fromEvent($("#practiceInput"), "keyup")
+rxjs
+  .fromEvent($("#practiceInput"), "keyup")
   .pipe(
-    rxjs.operators.pluck('target', 'value'),
+    rxjs.operators.pluck("target", "value"),
     rxjs.operators.distinct(),
     rxjs.operators.debounceTime(2000),
-    rxjs.operators.mergeMap(v => rxjs.from(getUserById(v))),
-    rxjs.operators.catchError(err => rxjs.of(err)),
-    rxjs.operators.map(x => x.response[0]),
+    rxjs.operators.mergeMap((v) => rxjs.from(getUserById(v))),
+    rxjs.operators.catchError((err) => rxjs.of(err)),
+    rxjs.operators.map((x) => x.response[0])
   )
-  .subscribe((user) => {
-      $('#practiceText').html(`${user.first_name} ${user.last_name} <i>${user.id}</i>`);
-      $('#practicePic').attr('src', user.photo_big);
-      console.log(user)
-    }, 
-    err => console.log(err),
-    () => console.log('Completed')
+  .subscribe(
+    (user) => {
+      $("#practiceText").html(
+        `${user.first_name} ${user.last_name} <i>${user.id}</i>`
+      );
+      $("#practicePic").attr("src", user.photo_big);
+      console.log(user);
+    },
+    (err) => console.log(err),
+    () => console.log("Completed")
   );
